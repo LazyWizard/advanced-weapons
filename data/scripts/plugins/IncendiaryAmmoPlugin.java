288aefb4689915760268ff3eaa8402adff7cb351
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-03-31 19:27:31 -0500

    Rewrote IncendiaryAmmoPlugin to no longer store fires as static variables

--- data/scripts/plugins/IncendiaryAmmoPlugin.java
+++ data/scripts/plugins/IncendiaryAmmoPlugin.java
@@ -7,10 +7,10 @@ import com.fs.starfarer.api.combat.DamageType;
 import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
 import java.awt.Color;
 import java.lang.ref.WeakReference;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.WeakHashMap;
 import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.combat.entities.AnchoredEntity;
@@ -19,14 +19,89 @@ import org.lwjgl.util.vector.Vector2f;
 // TODO: Check for nearby fires and merge them for better performance
 public class IncendiaryAmmoPlugin implements EveryFrameCombatPlugin
 {
-    // How long between damage/particle effect ticks
     private static final float TIME_BETWEEN_DAMAGE_TICKS = .2f;
-    private static final float TIME_BETWEEN_PARTICLE_TICKS = .8f;
+    private static final float TIME_BETWEEN_PARTICLE_TICKS = .4f;
+    private static WeakReference currentInstance;
     // Stores the currently burning fires
-    // Having the Set backed by a WeakHashMap helps prevent memory leaks
-    private static final Set burning = Collections.newSetFromMap(new WeakHashMap());
+    // Using a WeakHashMap helps prevent memory leaks
+    private final Map burning = new WeakHashMap();
+    private boolean shouldMergeFires = false;
     private CombatEngineAPI engine;
     private float lastDamage, lastParticle;
+
+    public static void startFire(CombatEntityAPI target, Vector2f hitLoc,
+            float totalDamage, float burnDuration, CombatEntityAPI source)
+    {
+        if (getInstance() != null)
+        {
+            getInstance().startFireActual(target, hitLoc, totalDamage, burnDuration, source);
+        }
+    }
+
+    public static void stopFires(CombatEntityAPI target)
+    {
+        if (getInstance() != null)
+        {
+            getInstance().stopFires(target);
+        }
+    }
+
+    private static void stopFiresInArea(CombatEntityAPI target, Vector2f loc, float radius)
+    {
+        if (getInstance() != null)
+        {
+            getInstance().stopFiresInArea(target, loc, radius);
+        }
+    }
+
+    private static IncendiaryAmmoPlugin getInstance()
+    {
+        if (currentInstance == null || currentInstance.get() == null)
+        {
+            return null;
+        }
+
+        return (IncendiaryAmmoPlugin) currentInstance.get();
+    }
+
+    private void startFireActual(CombatEntityAPI target, Vector2f hitLoc,
+            float totalDamage, float burnDuration, CombatEntityAPI source)
+    {
+        List fires;
+
+        if (burning.containsKey(target))
+        {
+            fires = (List) burning.get(target);
+            shouldMergeFires = true;
+        }
+        else
+        {
+            fires = new ArrayList();
+            burning.put(target, fires);
+        }
+
+        // TODO: merge with nearby fires on the same target
+        fires.add(new FireData(target, hitLoc, totalDamage, burnDuration, source));
+    }
+
+    private void stopFiresActual(CombatEntityAPI target)
+    {
+        if (burning.containsKey(target))
+        {
+            List fires = (List) burning.get(target);
+            for (Iterator iter = fires.iterator(); iter.hasNext();)
+            {
+                FireData tmp = (FireData) iter.next();
+                iter.remove();
+            }
+        }
+    }
+
+    // TODO
+    private void stopFiresInAreaActual(CombatEntityAPI target, Vector2f loc, float radius)
+    {
+
+    }
 
     @Override
     public void advance(float amount, List events)
@@ -56,62 +131,60 @@ public class IncendiaryAmmoPlugin implements EveryFrameCombatPlugin
         }
 
         // Deal fire damage for all actively burning projectiles
-        for (Iterator iter = burning.iterator(); iter.hasNext();)
+        for (Iterator iter = burning.values().iterator(); iter.hasNext();)
         {
-            FireData fire = (FireData) iter.next();
-
-            // Check if the fire has gone out
-            if (engine.getTotalElapsedTime(false) >= fire.expiration
-                    || !engine.isEntityInPlay(fire.getAnchor()))
+            List fires = (List) iter.next();
+            if (fires.isEmpty())
             {
                 iter.remove();
+                continue;
             }
-            else
-            {
-                if (dealDamage)
-                {
-                    engine.applyDamage(fire.getAnchor(), fire.getLocation(),
-                            fire.dps * damageMod, DamageType.FRAGMENTATION,
-                            fire.dps * damageMod, true, true, fire.source.get());
-                }
 
-                // Draw smoke effect to show where the fire is burning
-                if (showParticle)
+            if (shouldMergeFires && fires.size() > 1)
+            {
+                // TODO
+                System.out.println("Would merge fires now.");
+            }
+
+            for (Iterator iter2 = fires.iterator(); iter2.hasNext();)
+            {
+                FireData fire = (FireData) iter2.next();
+
+                if (engine.getTotalElapsedTime(false) >= fire.expiration
+                        || !engine.isEntityInPlay(fire.getAnchor()))
                 {
-                    engine.addSmokeParticle(fire.getLocation(), // Location
-                            MathUtils.getRandomPointOnCircumference(null, 5f), // Velocity
-                            MathUtils.getRandomNumberInRange(20f, 40f), // Size
-                            MathUtils.getRandomNumberInRange(.05f, .15f), // Brightness
-                            3f, Color.DARK_GRAY); // Duration, color
+                    iter2.remove();
+                }
+                else
+                {
+                    if (dealDamage)
+                    {
+                        engine.applyDamage(fire.getAnchor(), fire.getLocation(),
+                                fire.dps * damageMod, DamageType.FRAGMENTATION,
+                                fire.dps * damageMod, true, true, fire.source.get());
+                    }
+
+                    // Draw smoke effect to show where the fire is burning
+                    if (showParticle)
+                    {
+                        engine.addSmokeParticle(fire.getLocation(), // Location
+                                MathUtils.getRandomPointOnCircumference(null, 5f), // Velocity
+                                MathUtils.getRandomNumberInRange(20f, 40f), // Size
+                                MathUtils.getRandomNumberInRange(.05f, .15f), // Brightness
+                                3f, Color.DARK_GRAY); // Duration, color
+                    }
                 }
             }
         }
-    }
 
-    public static void startFire(CombatEntityAPI target, Vector2f hitLoc,
-            float totalDamage, float burnDuration, CombatEntityAPI source)
-    {
-        // TODO: merge with nearby fires on the same target
-        burning.add(new FireData(target, hitLoc, totalDamage, burnDuration, source));
-    }
-
-    public static void stopFire(CombatEntityAPI target)
-    {
-        for (Iterator iter = burning.iterator(); iter.hasNext();)
-        {
-            FireData tmp = (FireData) iter.next();
-            if (target == tmp.getAnchor())
-            {
-                iter.remove();
-            }
-        }
+        shouldMergeFires = false;
     }
 
     @Override
     public void init(CombatEngineAPI engine)
-    {
+    {;
         this.engine = engine;
-        burning.clear();
+        currentInstance = new WeakReference(this);
     }
 
     private static class FireData
@@ -120,7 +193,7 @@ public class IncendiaryAmmoPlugin implements EveryFrameCombatPlugin
         private final WeakReference source;
         private final float dps, expiration;
 
-        public FireData(CombatEntityAPI target, Vector2f hitLoc,
+        private FireData(CombatEntityAPI target, Vector2f hitLoc,
                 float totalDamage, float burnDuration, CombatEntityAPI source)
         {
             this.hitLoc = new AnchoredEntity(target, hitLoc);
