77ecf15a84673ee1c07e091fe1f9991cc0d395c7
LazyWizard <iu65a@hotmail.com>
2014-04-12 18:47:24 -0500

    Slashing beams now turn with ship and return to original facing when done

--- data/scripts/weapons/SlashBeamEffect.java
+++ data/scripts/weapons/SlashBeamEffect.java
@@ -3,25 +3,34 @@ package data.scripts.weapons;
 import com.fs.starfarer.api.combat.BeamAPI;
 import com.fs.starfarer.api.combat.BeamEffectPlugin;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.WeaponAPI;
 import org.lazywizard.lazylib.MathUtils;
 
-// TODO: Reset weapon facing at end of beam (if possible)
-// TODO: Fix beams 'sticking' if fired at edge of their arc (exploitable!)
+// Freespace 2-styled slashing beams
+// This is meant for burst beams and does not obey weapon turn speed
+// TODO: Fix beams 'sticking' if fired at edge of their arc (extremely exploitable!)
 // TODO: Tie slash size/duration into something easily editable
+// TODO: The code could use some cleanup
 public class SlashBeamEffect implements BeamEffectPlugin
 {
     private float relativeArcStart, arcSize, startTime,
             duration, direction;
-    private boolean newBeam = true;
+    private boolean newBeam = true, slashDone = false;
 
     @Override
     public void advance(float amount, CombatEngineAPI engine, BeamAPI beam)
     {
+        if (slashDone)
+        {
+            return;
+        }
+
         WeaponAPI weapon = beam.getWeapon();
+        ShipAPI ship = weapon.getShip();
         float curTime = engine.getTotalElapsedTime(false);
 
-        // First frame: calculate the details of the slash
+        // First frame: calculate the details of the slashing motion
         if (newBeam)
         {
             // Whether the beam travels right or left
@@ -31,7 +40,7 @@ public class SlashBeamEffect implements BeamEffectPlugin
             // The angle of the beginning of the slash
             // Ship facing is used to compensate for turning while firing
             relativeArcStart = (weapon.getCurrAngle() - (arcSize / 2f * direction))
-                    - weapon.getShip().getFacing();
+                    - ship.getFacing();
             // The time the slash started (used to calculate slash progress)
             startTime = curTime;
             // How long the slash lasts (used to calculate slash speed)
@@ -40,10 +49,22 @@ public class SlashBeamEffect implements BeamEffectPlugin
             newBeam = false;
         }
 
-        // Percentage of the slash that is complete
-        float slashProgress = (curTime - startTime) / duration;
-        System.out.println("Slash progress: " + (slashProgress * 100f) + "%");
-        weapon.setCurrAngle(MathUtils.clampAngle((relativeArcStart
-                + (arcSize * slashProgress * direction)) + weapon.getShip().getFacing()));
+        // How long the beam has been active
+        float elapsed = curTime - startTime;
+
+        // If beam is finished, move weapon back to original facing
+        if (elapsed >= (duration - .1f))
+        {
+            weapon.setCurrAngle(MathUtils.clampAngle(relativeArcStart +
+                    (arcSize / 2f * direction) + ship.getFacing()));
+            slashDone = true;
+        }
+        // Otherwise, continue the slashing motion
+        else
+        {
+            weapon.setCurrAngle(MathUtils.clampAngle((relativeArcStart
+                    + (arcSize * (elapsed / duration) * direction))
+                    + ship.getFacing()));
+        }
     }
 }
